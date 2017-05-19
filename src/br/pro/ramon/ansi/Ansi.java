12104d63fd04355eacd151b6e3fe29c522966a8c
Ramon Chiara <ramon@ramon.pro.br>
2017-05-18 22:03:48 -0300

    Criação de uma classe ("módulo") específica para as funções.

---
+++ src/br/pro/ramon/ansi/Ansi.java
@@ -0,0 +1,95 @@
+package br.pro.ramon.ansi;
+
+public class Ansi {
+
+    public static void limpaTela() {
+        System.out.printf("\u001b[2J");
+    }
+
+    public static void reset() {
+        System.out.printf("\u001b[0m");
+    }
+
+    public static void negrito(boolean ligado) {
+        if (ligado) {
+            System.out.printf("\u001b[1m");
+        } else {
+            System.out.printf("\u001b[22m");
+        }
+    }
+
+    public static void cor(String cor) {
+        int valor = 0;
+
+        switch (cor.toLowerCase()) {
+            case "preto":
+            case "preta":
+                valor = 30;
+                break;
+            case "vermelho":
+            case "vermelha":
+                valor = 31;
+                break;
+            case "verde":
+                valor = 32;
+                break;
+            case "amarelo":
+            case "amarela":
+                valor = 33;
+                break;
+            case "azul":
+                valor = 34;
+                break;
+            case "magenta":
+                valor = 35;
+                break;
+            case "ciano":
+                valor = 36;
+                break;
+            case "branco":
+            case "branca":
+                valor = 37;
+                break;
+        }
+
+        System.out.printf("\u001b[%dm", valor);
+    }
+
+    public static void fundo(String cor) {
+        int valor = 0;
+
+        switch (cor.toLowerCase()) {
+            case "preto":
+            case "preta":
+                valor = 40;
+                break;
+            case "vermelho":
+            case "vermelha":
+                valor = 41;
+                break;
+            case "verde":
+                valor = 42;
+                break;
+            case "amarelo":
+            case "amarela":
+                valor = 43;
+                break;
+            case "azul":
+                valor = 44;
+                break;
+            case "magenta":
+                valor = 45;
+                break;
+            case "ciano":
+                valor = 46;
+                break;
+            case "branco":
+            case "branca":
+                valor = 47;
+                break;
+        }
+
+        System.out.printf("\u001b[%dm", valor);
+    }
+
+}
--- src/br/pro/ramon/ansi/Program.java
+++ src/br/pro/ramon/ansi/Program.java
@@ -1,5 +1,11 @@
 package br.pro.ramon.ansi;
 
+import static br.pro.ramon.ansi.Ansi.cor;
+import static br.pro.ramon.ansi.Ansi.fundo;
+import static br.pro.ramon.ansi.Ansi.limpaTela;
+import static br.pro.ramon.ansi.Ansi.negrito;
+import static br.pro.ramon.ansi.Ansi.reset;
+
 public class Program {
 
     public static void main(String[] args) {
@@ -46,96 +52,6 @@ public class Program {
         reset();
         System.out.println();
         System.out.println("De volta ao \"normal\"...");
-    }
-
-    public static void limpaTela() {
-        System.out.printf("\u001b[2J");
-    }
-
-    public static void reset() {
-        System.out.printf("\u001b[0m");
-    }
-
-    public static void negrito(boolean ligado) {
-        if (ligado) {
-            System.out.printf("\u001b[1m");
-        } else {
-            System.out.printf("\u001b[22m");
-        }
-    }
-
-    public static void cor(String cor) {
-        int valor = 0;
-
-        switch (cor.toLowerCase()) {
-            case "preto":
-            case "preta":
-                valor = 30;
-                break;
-            case "vermelho":
-            case "vermelha":
-                valor = 31;
-                break;
-            case "verde":
-                valor = 32;
-                break;
-            case "amarelo":
-            case "amarela":
-                valor = 33;
-                break;
-            case "azul":
-                valor = 34;
-                break;
-            case "magenta":
-                valor = 35;
-                break;
-            case "ciano":
-                valor = 36;
-                break;
-            case "branco":
-            case "branca":
-                valor = 37;
-                break;
-        }
-
-        System.out.printf("\u001b[%dm", valor);
-    }
-
-    public static void fundo(String cor) {
-        int valor = 0;
-
-        switch (cor.toLowerCase()) {
-            case "preto":
-            case "preta":
-                valor = 40;
-                break;
-            case "vermelho":
-            case "vermelha":
-                valor = 41;
-                break;
-            case "verde":
-                valor = 42;
-                break;
-            case "amarelo":
-            case "amarela":
-                valor = 43;
-                break;
-            case "azul":
-                valor = 44;
-                break;
-            case "magenta":
-                valor = 45;
-                break;
-            case "ciano":
-                valor = 46;
-                break;
-            case "branco":
-            case "branca":
-                valor = 47;
-                break;
-        }
-
-        System.out.printf("\u001b[%dm", valor);
     }
 
 }
